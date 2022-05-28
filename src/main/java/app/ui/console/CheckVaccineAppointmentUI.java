7675ef8a32f7870c279653805d05def3d56973ad
Pedro Nogueira <1211613@isep.ipp.pt>
2022-05-29 00:19:32 +0100

    CheckVaccineAppoitmentUI.java - added a validation for SNS User Number.
    US004.java - tests for waiting room addition method.

--- src/main/java/app/ui/console/CheckVaccineAppointmentUI.java
+++ src/main/java/app/ui/console/CheckVaccineAppointmentUI.java
@@ -2,6 +2,7 @@ package app.ui.console;
 
 import app.controller.CheckVaccineAppointmentController;
 import app.domain.shared.Constants;
+import app.domain.shared.Validate;
 import app.ui.console.utils.Utils;
 
 import java.lang.module.FindException;
@@ -25,8 +26,23 @@ public class CheckVaccineAppointmentUI implements Runnable {
 
         System.out.printf("------------------------- Check vaccine appointment -------------------------%n%n");
 
-        System.out.print("SNS User number: ");
-        int number = ler.nextInt();
+        int number = 0;
+        boolean success;
+        do {
+            success = true;
+            try {
+                System.out.print("SNS User number: ");
+                number = ler.nextInt();
+                    if (!Validate.validateSNS(number)) {
+                        System.out.println("Invalid SNS User number!");
+                        success = false;
+                    }
+            } catch (Exception e) {
+                System.out.println("Invalid SNS User number!");
+                success = false;
+                ler.next();
+            }
+        } while (!success);
 
         if (checkSnsUserAppointment(number)) {
             System.out.printf("SNS User: %d%n", ctrl.getSnsUserAppointment().getSNSnumber());
@@ -34,10 +50,13 @@ public class CheckVaccineAppointmentUI implements Runnable {
             System.out.printf("Appointment Time: %s%n", ctrl.getSnsUserAppointment().getAppointmentTime().format(Constants.DATE_TIME_FORMATTER));
             System.out.println("---------------------------------------------------------------------------");
             if (ctrl.requestSnsUserArrivalRegistration()) {
-                System.out.printf("Arrival:%nSNS User: %s%nTime of arrival: %s%n%n",
-                        ctrl.getArrival().getSnSuser().getName(), ctrl.getArrival().getTimeOfArrival().format(Constants.DATE_TIME_FORMATTER));
-                if (ctrl.requestToSaveSnsUserArrival()) {
-                    System.out.println("------------------------- Successfully registered SNS User arrival and added to waiting list -------------------------");
+                System.out.printf("Arrival:%nSNS User: %s%nTime of arrival: %s%n%n", ctrl.getArrival().getSnSuser().getName(), ctrl.getArrival().getTimeOfArrival().format(Constants.TIME_FORMATTER));
+                if (Utils.confirm("Confirm Data? (s or n)")) {
+                    if (ctrl.requestToSaveSnsUserArrival()) {
+                        System.out.println("------------------------- Successfully registered SNS User arrival and added to waiting list -------------------------");
+                    }
+                } else {
+                    System.out.println("--------Process cancelled--------");
                 }
             }
         }
@@ -45,12 +64,12 @@ public class CheckVaccineAppointmentUI implements Runnable {
 
     private boolean checkSnsUserAppointment(int number) {
         if (!ctrl.SNSuserExists(number)) {
-            System.out.println("SNS User is not on the system");
+            System.out.println("SNS User is not in the system");
             return false;
         } else if (!ctrl.checkAppointment()) {
             System.out.println("There are no appointments for this SNS User!");
             return false;
-        } else if (!ctrl.checkAppointmentDay()) {
+        } else if (ctrl.checkAppointmentDay()) {
             System.out.println("Appointment is scheduled for a different day!");
             return false;
         }
