09238cab7d73f6e3d832a8173410bb1300399d2f
1201082_JoaoVeiga <1201082@isep.ipp.pt>
2022-05-29 12:00:39 +0100

    Fix US[004] test error of Constant call

--- src/main/java/tests/US004.java
+++ src/main/java/tests/US004.java
@@ -8,6 +8,8 @@ import app.domain.model.SNSuser;
 import app.domain.shared.Constants;
 import org.junit.jupiter.api.Test;
 
+import java.util.concurrent.CompletionService;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class US004 {
@@ -15,18 +17,15 @@ class US004 {
     @Test
     void addSnsUserToWaitingList() {
         //Arrange
-        SNSuser snSuser = Constants.SN_SUSER_TESTER;
+        SNSuser snSuser = Constants.SN_SUSER_TESTER_FULL;
         Arrival arrival = new Arrival(snSuser);
         Company company = App.getInstance().getCompany();
+        //Act
         company.addSnsUserToWaitingList(0, arrival);
         int test = company.getWaitingList(0).size();
         int expected = 1;
-
-        //Act
-        boolean result = test == expected;
-
         //Assert
-        assertTrue(result);
+        assertEquals(test,expected);
     }
 
 
