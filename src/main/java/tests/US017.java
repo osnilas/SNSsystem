dff1fc291a61a661c76a6100420b29fa7caf10ac
filipe_magalhaes <1211606@isep.ipp.pt>
2022-06-18 19:45:07 +0100

    US17: started tests for this US

---
+++ src/main/java/tests/US017.java
@@ -0,0 +1,32 @@
+package tests;
+
+import app.domain.model.LegacySystemData;
+import app.domain.model.ReadDataFromLegacySystem;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class US017 {
+
+    @Test
+    void copyDataFromLegacySystemValid() {
+        //Arrange
+        ArrayList<LegacySystemData> expected = new ArrayList<>();
+        //Act
+
+        //Assert
+
+    }
+
+    @Test
+    void copyDataFromLegacySystemInvalid() {
+        //Arrange
+
+        //Act
+
+        //Assert
+
+    }
+}
